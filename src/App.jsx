fdf0a08b85d81164025ae4175e09c610ef5a5428
dullarzee <timilehin0303@gmail.com>
2025-02-16 16:05:40 +0100

    improved solution ...deployed to netlify already before now and works well

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import divider from '/images/pattern-divider-desktop.svg'
+import dice from '/images/icon-dice.svg'
 import './tailwind/App.css'
 import PropTypes from 'prop-types'
 
@@ -24,7 +26,7 @@ function Advice({data, count}){
 function ButtonDice({ setCount }){
   return(
     <button onClick={()=>setCount(Math.floor(Math.random()*49))} className="absolute top-[85%] lg:top-[90%] left-[40%] lg:left-[44%] flex justify-center p-[5%] lg:p-[3.4%] cursor-pointer hover:shadow-heading hover:shadow-button w-[4rem] h-[4rem] bg-heading rounded-full">
-     <img alt="click to switch advice" className="" src="/images/icon-dice.svg"></img>
+     <img alt="click to switch advice" className="" src={dice}></img>
     </button>
   );
 }
@@ -47,7 +49,7 @@ function App() {
     <section className="relative w-[90%] lg:w-[40%] pt-[10%] pb-[10%] lg:p-[2.5%] mx-auto bg-cardBg rounded-[10px] mt-[30%] lg:mt-[10%]">
       <Heading count={count}/>
         <Advice data={data} count={count} />
-          <img alt="" src="/images/pattern-divider-desktop.svg" className="mx-auto my-[1.5rem] lg:my-[3.3rem]"></img>
+          <img alt="" src={divider} className="mx-auto my-[1.5rem] lg:my-[3.3rem]"></img>
           <ButtonDice setCount={setCount} />
     </section>
     </>
